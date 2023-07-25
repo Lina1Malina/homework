b1e26c5c90ef8fe03a5bdf152516997de5d244ab
Lina <ananasik.lina@mail.rugit config -- global use.nameq>
2023-07-25 23:33:37 +0300

    Add task06

---
+++ task06/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt16(Console.ReadLine());
+
+if (number < 0)
+{
+    number *= -1;
+}
+if ((number % 2 == 0))
+{
+    int result = number % 2;
+    Console.WriteLine("Число четное ");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
