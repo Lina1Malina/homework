119502d97dad4d857bb9b26f3c2f305a39c59c0d
Lina <ananasik.lina@mail.rugit config -- global use.nameq>
2023-07-25 22:28:13 +0300

    Add tast02

---
+++ task02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите число:");
+int number1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int number2 = Convert.ToInt16(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"{number1} большее число. {number2} меньшее.");
+}
+else 
+{
+    Console.WriteLine($"{number2} большее число. {number1} меньшее.");
+}
