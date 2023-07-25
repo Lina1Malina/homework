bce6ade6594bcea727980b392163a1f3a1912b67
Lina <ananasik.lina@mail.rugit config -- global use.nameq>
2023-07-25 20:35:55 +0300

    Add task07

---
+++ task07/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите трехзначное целое число");
+int number = Convert.ToInt16(Console.ReadLine());
+
+if (number < 0)
+{
+    number *= -1;
+}
+
+if (number >= 100 && number <= 999)
+{
+    int result = number % 10;
+    //int result = number / 100; первая цифра трехзначного числа
+    // int result = number / 10 % 10; вторая цифра трехзначного числа
+    Console.WriteLine($"Последняя цифра числа: {result}");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
+
