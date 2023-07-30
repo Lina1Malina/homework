adc64010025ba7bcf4ba2eb00ee1630e9bf11f51
Lina <ananasik.lina@mail.rugit config -- global use.nameq>
2023-07-31 01:46:02 +0300

    add task10.13.15

---
+++ task15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+void week (int number)
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("этот день - выходной");
+    }
+    else if (number < 1 || number >7) 
+    {
+        Console.WriteLine("введите число от 1 до 7");
+    }
+    else 
+    Console.WriteLine("этот день не выходной");
+}
+week(number);
